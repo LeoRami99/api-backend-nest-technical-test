8c8309427bf7ea3fa0f7b8f68db7589aee41afd6
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-20 01:18:38 -0500

    Add ApiExternalModule to TransactionsModule imports

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -4,9 +4,10 @@ import { TransactionsController } from './transactions.controller';
 import { TransactionModel } from '../models/transaction.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { SequelizeTransactionRepositoryAdapter } from './adapters/sequelize-transaction.repository.adapter';
+import { ApiExternalModule } from '../api-external/api-external.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([TransactionModel])],
+  imports: [SequelizeModule.forFeature([TransactionModel]), ApiExternalModule],
   controllers: [TransactionsController],
   providers: [
     TransactionsService,
