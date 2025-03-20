5f450b439a3f1b32a2fe0e9ac3d3ab9ebce32fe6
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-19 23:18:30 -0500

    Fix import path for SequelizeProductRepositoryAdapter in products.module.ts

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,7 +2,7 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ProductModel } from '../models/product.model';
-import { SequelizeProductRepositoryAdapter } from '../adapters/sequelize-product.repository.adapter';
+import { SequelizeProductRepositoryAdapter } from './adapters/sequelize-product.repository.adapter';
 
 import { Module } from '@nestjs/common';
 
