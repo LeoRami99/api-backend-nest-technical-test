456f34c661a9390353d58f087fba386f35509f37
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-19 23:19:08 -0500

    Add Sequelize integration and repository adapter to TransactionsModule

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { TransactionModel } from '../models/transaction.model';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeTransactionRepositoryAdapter } from './adapters/sequelize-transaction.repository.adapter';
 
 @Module({
+  imports: [SequelizeModule.forFeature([TransactionModel])],
   controllers: [TransactionsController],
-  providers: [TransactionsService],
+  providers: [
+    TransactionsService,
+    {
+      provide: 'TransactionRepository',
+      useClass: SequelizeTransactionRepositoryAdapter,
+    },
+  ],
 })
 export class TransactionsModule {}
