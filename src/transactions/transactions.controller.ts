adeb4ed661d8dcb35cea20f4a723170b5580264e
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-19 23:19:03 -0500

    Add createTransaction method to TransactionsController for handling transaction creation

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
+  @Post('/')
+  createTransaction(@Body() createTransactionDTO: CreateTransactionDto) {
+    return this.transactionsService.createTransaction(createTransactionDTO);
+  }
 }
