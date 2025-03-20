852b67cb6b24fadde8e4edb952646bbaaeff4f3c
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-20 01:18:42 -0500

    Integrate ApiExternalService into TransactionsService for external data retrieval

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,15 +1,19 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { TransactionRepository } from './ports/transaction.repository';
 import { Transaction } from './core/domain/transaction.entity';
+import { ApiExternalService } from '../api-external/api-external.service';
 
 @Injectable()
 export class TransactionsService {
   constructor(
     @Inject('TransactionRepository')
     private readonly transactionRepository: TransactionRepository,
+    private readonly apiExternalService: ApiExternalService,
   ) {}
 
   async createTransaction(transaction: Transaction): Promise<Transaction> {
+    const data = await this.apiExternalService.getDataMerchants();
+    console.log(data.data);
     const transactionData =
       await this.transactionRepository.create(transaction);
     if (!transactionData) {
