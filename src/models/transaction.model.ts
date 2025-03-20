2d56652b8d8933fec0993a85e3df2fded8670b75
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-19 23:18:22 -0500

    Refactor Transaction model to TransactionModel and enhance fields with appropriate data types and default values

--- src/models/transaction.model.ts
+++ src/models/transaction.model.ts
@@ -1,9 +1,18 @@
 import { Model, Column, Table, DataType } from 'sequelize-typescript';
+
+// id: string,
+// amount: number,
+// status: string,
+// userId: string,
+// methodPayment: string,
+// productId: string,
+// price: number,
+
 @Table({
   tableName: 'transactions',
   timestamps: true,
 })
-class Transaction extends Model<Transaction> {
+class TransactionModel extends Model<TransactionModel> {
   @Column({
     type: DataType.UUID,
     primaryKey: true,
@@ -17,12 +26,31 @@ class Transaction extends Model<Transaction> {
   })
   amount: number;
   @Column({
+    type: DataType.ENUM('pending', 'approved', 'rejected', 'canceled'),
+    allowNull: false,
+    defaultValue: 'pending',
+  })
+  status: string;
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+  })
+  userId: string;
+  @Column({
     type: DataType.STRING,
     allowNull: false,
   })
-  status: string;
-  @Column
-  userId: string;
+  methodPayment: string;
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+  })
+  productId: string;
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  price: number;
 }
 
-export { Transaction };
+export { TransactionModel };
