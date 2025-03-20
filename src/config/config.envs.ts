0588e831598a72e8622fdefbc7a48d8744517615
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-20 01:18:27 -0500

    Add environment variables for API external service configuration

--- src/config/config.envs.ts
+++ src/config/config.envs.ts
@@ -6,4 +6,9 @@ export const getEnvs = (configService: ConfigService) => ({
   DB_USER: configService.get<string>('DB_USER'),
   DB_PASS: configService.get<string>('DB_PASS'),
   DB_NAME: configService.get<string>('DB_NAME'),
+
+  // Varibales for API external
+  API_EXTERNAL_URL_SANDBOX: configService.get<string>('UAT_SANDBOX_URL'),
+  PUBLIC_KEY_SANDBOX: configService.get<string>('PUBLIC_KEY'),
+  PRIVATE_KEY_SANDBOX: configService.get<string>('PRIVATE_KEY'),
 });
