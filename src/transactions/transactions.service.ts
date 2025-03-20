d4d06e27dee8292c9313514dd66ff6302813b495
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-19 23:19:13 -0500

    Implement createTransaction method in TransactionsService with repository injection

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { TransactionRepository } from './ports/transaction.repository';
+import { Transaction } from './core/domain/transaction.entity';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+  constructor(
+    @Inject('TransactionRepository')
+    private readonly transactionRepository: TransactionRepository,
+  ) {}
+
+  async createTransaction(transaction: Transaction): Promise<Transaction> {
+    const transactionData =
+      await this.transactionRepository.create(transaction);
+    if (!transactionData) {
+      throw new Error('Transaction could not be created');
+    }
+    return transactionData;
+  }
+}
