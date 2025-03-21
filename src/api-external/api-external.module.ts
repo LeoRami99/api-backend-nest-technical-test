63fa4bd6a9368dd45718a47fe7081ad593f658e0
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-21 02:01:24 -0500

    Add repository adapters for card and transaction external services in ApiExternalModule

--- src/api-external/api-external.module.ts
+++ src/api-external/api-external.module.ts
@@ -2,11 +2,27 @@ import { Module } from '@nestjs/common';
 import { ApiExternalService } from './api-external.service';
 import { ApiExternalController } from './api-external.controller';
 import { HttpModule } from '@nestjs/axios';
+import { CardRepositoryAdapter } from './adapters/card-repository.adapter';
+import { TransactionExternalRepositoryAdapter } from './adapters/transaction-repository.adapter';
 
 @Module({
   imports: [HttpModule],
   controllers: [ApiExternalController],
-  providers: [ApiExternalService],
-  exports: [ApiExternalService],
+  providers: [
+    ApiExternalService,
+    {
+      provide: 'CardRepository',
+      useClass: CardRepositoryAdapter,
+    },
+    {
+      provide: 'TransactionExternalRepository',
+      useClass: TransactionExternalRepositoryAdapter,
+    },
+  ],
+  exports: [
+    ApiExternalService,
+    'CardRepository',
+    'TransactionExternalRepository',
+  ],
 })
 export class ApiExternalModule {}
