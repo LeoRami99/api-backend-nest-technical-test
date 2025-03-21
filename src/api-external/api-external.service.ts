e5d0150dff89959c4e0b8068f7f08b06c4d20358
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-21 02:01:28 -0500

    Add methods for token card retrieval and external transaction creation in ApiExternalService

--- src/api-external/api-external.service.ts
+++ src/api-external/api-external.service.ts
@@ -1,8 +1,19 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
 import { getEnvs } from '../config/config.envs';
+import { Card } from './core/domain/card-entity';
+
+import { ApiCardResult } from './interfaces/api-card-result.interface';
+import { ApiTransactionExternalResult } from './interfaces/api-transaction-result.interface';
+import { TransactionExternal } from './core/domain/transaction-entity';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class ApiExternalService {
@@ -24,6 +35,56 @@ export class ApiExternalService {
       return response.data;
     } catch {
       throw new Error('Could not get data from external API');
+    }
+  }
+
+  async getTokenCard(card: Card): Promise<ApiCardResult> {
+    try {
+      const envs = getEnvs(this.configService);
+      const url = envs.API_EXTERNAL_URL_SANDBOX;
+      const uri = `${url}/tokens/cards`;
+
+      const response = await firstValueFrom(
+        this.httpService.post<ApiCardResult>(uri, card, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${envs.PUBLIC_KEY_SANDBOX}`,
+          },
+        }),
+      );
+      return response.data;
+    } catch {
+      throw InternalServerErrorException;
+    }
+  }
+
+  async createTransactionExternal(
+    transaction: TransactionExternal,
+  ): Promise<ApiTransactionExternalResult> {
+    try {
+      const envs = getEnvs(this.configService);
+      const url = envs.API_EXTERNAL_URL_SANDBOX;
+      const uri = `${url}/transactions`;
+
+      const response = await firstValueFrom(
+        this.httpService.post<ApiTransactionExternalResult>(uri, transaction, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${envs.PRIVATE_KEY_SANDBOX}`,
+          },
+        }),
+      );
+      return response.data;
+    } catch (error: any) {
+      const axiosError = error as AxiosError;
+      console.error(
+        'Error in createTransactionExternal:',
+        axiosError.response?.data || axiosError.message,
+      );
+      throw new HttpException(
+        axiosError.response?.data || axiosError.message,
+        axiosError.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
