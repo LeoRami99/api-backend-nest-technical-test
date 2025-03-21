563e4f7a7ffc512dbc668acd4f7fc611e51cd31e
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-21 02:01:19 -0500

    Add endpoints for retrieving merchants and token card in ApiExternalController

--- src/api-external/api-external.controller.ts
+++ src/api-external/api-external.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiExternalService } from './api-external.service';
+import { GetDataCardDto } from './dto/card.dto';
 
 @Controller('api-external')
 export class ApiExternalController {
   constructor(private readonly apiExternalService: ApiExternalService) {}
+
+  @Get('/merchants')
+  async getDataMerchants() {
+    return this.apiExternalService.getDataMerchants();
+  }
+  @Post('/token-card')
+  async getTokenCard(@Body() card: GetDataCardDto) {
+    return this.apiExternalService.getTokenCard(card);
+  }
 }
