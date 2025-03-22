935ddfd4c6d93bd99fc4ddc92f9a9cdb7d7bca69
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-22 11:13:32 -0500

    Add updateInternalByExternalId method to TransactionRepository interface

--- src/transactions/ports/transaction.repository.ts
+++ src/transactions/ports/transaction.repository.ts
@@ -2,4 +2,8 @@ import { Transaction } from '../core/domain/transaction.entity';
 
 export interface TransactionRepository {
   create(transaction: Transaction): Promise<Transaction>;
+  updateInternalByExternalId(
+    idExternalTransaction: string,
+    status: string,
+  ): Promise<Transaction>;
 }
