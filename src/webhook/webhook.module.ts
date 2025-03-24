6f9684cb34e6f51401830305cfb238742e21c52a
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-23 21:52:40 -0500

    Add ProductsModule to WebhookModule imports for product-related webhook functionality

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -3,9 +3,10 @@ import { WebhookService } from './webhook.service';
 import { WebhookController } from './webhook.controller';
 import { SequelizeWebhookRepositoryAdapter } from './adapters/sequelize-webhook.repository.adapter';
 import { TransactionsModule } from 'src/transactions/transactions.module';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [TransactionsModule, ProductsModule],
   controllers: [WebhookController],
   providers: [
     WebhookService,
