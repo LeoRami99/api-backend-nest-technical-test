e3cdd44737779fbe284e680b1d793feeb7cc171d
Leonardo Ramírez <juanlov4321@gmail.com>
2025-03-25 22:50:52 -0500

    Add SSL configuration to database connection for secure communication

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,12 @@ import { WebhookModule } from './webhook/webhook.module';
         database: getEnvs(configService).DB_NAME,
         autoLoadModels: true,
         synchronize: true,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
       }),
     }),
     ProductsModule,
